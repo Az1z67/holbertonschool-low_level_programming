31948610b08af64b6d8daa8031e341df5f48bdb2
Az1z67 <abbasovaziz61@gmail.com>
2024-02-23 01:02:20 +0400

    com

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -2,11 +2,14 @@
 #include "main.h"
 
 /**
+*_putchar - Writes a character to stdout
+*@c: The character to be printed
+*
 *print_alphabet - function print_alphabet _putchar
 *
 *Return: Always 0.
 */
-
+void _putchar(char c);
 void print_alphabet(void)
 {
 char i;
