2a66214d4b35da9bdd6f0f7d0b6b48f351892ef6
Az1z67 <abbasovaziz61@gmail.com>
2024-02-23 00:35:40 +0400

    com

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
@@ -7,16 +6,8 @@
 *Return: Always 0.
 **/
 
-int main(void)
-{
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+int main(void) {
+    printf("_putchar\n");
+    return 0;
 }
