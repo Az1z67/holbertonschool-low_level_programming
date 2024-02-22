f122c022c337fe53e900d82ba8a413ff797e1aad
Az1z67 <abbasovaziz61@gmail.com>
2024-02-23 01:39:05 +0400

    com

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+* _abs the absolute value of an integer
+*
+*@n: - checks int n
+* Return: 1 for letters. Returns 0 for rest
+*/
+
+
+int _abs(int n)
+{
+if (n > 0)
+{
+return (n);
+}
+else
+{
+return (-1 * n);
+}
+}
