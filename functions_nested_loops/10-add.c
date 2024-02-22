d619875f9f20ee9c8ce7e3afff428f136e130654
Az1z67 <abbasovaziz61@gmail.com>
2024-02-23 01:58:24 +0400

    com

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+*add -function that adds two integers
+*
+*@a:first parametr
+*
+*@b:seconds parametr
+*
+*Return: Always 0.
+*/
+
+
+int add(int a, int b)
+{
+return (a + b);
+}
