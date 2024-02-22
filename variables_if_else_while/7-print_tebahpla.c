6ebfc807f4e33492c796903f3bedd9316bf39ad8
Az1z67 <abbasovaziz61@gmail.com>
2024-02-22 23:37:31 +0400

    com

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+
+/**
+*main - function prints alphabet#include <stdio.h>
+/**
+*main - function prints alphabetly
+*
+*Return: Always 0.
+*/
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
+*
+*Return: Always 0.
+**/
+
+
+int main(void)
+{
+char c;
+for (c = 'z' ; c >= 'a' ; c--)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
