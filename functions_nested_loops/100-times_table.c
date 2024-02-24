b5b644fa40843e59532eb090ea52feece5b6633a
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 18:46:40 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,41 +2,28 @@
 #include <unistd.h>
 
 void print_times_table(int n) {
+    int i, j;
+
     if (n < 0 || n > 15)
         return;
 
-    for (int i = 0; i <= n; i++) {
-        for (int j = 0; j <= n; j++) {
+    for (i = 0; i <= n; i++) {
+        for (j = 0; j <= n; j++) {
             int result = i * j;
-            char buffer[10]; // Adjusted buffer size
+            char buffer[10]; /* Adjusted buffer size */
             int len;
 
             if (result < 10) {
-                buffer[0] = ' ';
-                buffer[1] = ' ';
-                buffer[2] = ' ';
-                buffer[3] = result + '0';
-                len = 4;
-            } else if (result < 100) {
-                buffer[0] = ' ';
-                buffer[1] = ' ';
-                buffer[2] = (result / 10) + '0';
-                buffer[3] = (result % 10) + '0';
-                len = 4;
+                buffer[0] = result + '0';
+                len = 1;
             } else {
-                buffer[0] = ' ';
-                buffer[1] = (result / 100) + '0';
-                buffer[2] = ((result / 10) % 10) + '0';
-                buffer[3] = (result % 10) + '0';
-                len = 4;
+                buffer[0] = result / 10 + '0';
+                buffer[1] = result % 10 + '0';
+                len = 2;
             }
 
             buffer[len++] = ',';
             buffer[len++] = ' ';
-
-            if (j != n)
-                buffer[len++] = ' ';
-
             buffer[len] = '\0';
 
             write(1, buffer, len);
