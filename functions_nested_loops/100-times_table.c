7608fcef135f6f8fe2cce95c61602541189a9572
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 18:23:22 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 void print_times_table(int n) {
+    int i, j; /* Переместим объявление переменных в начало функции */
+
     if (n < 0 || n > 15)
         return;
-
-    int i, j; // Переместим объявление переменных в начало функции
 
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++) {
