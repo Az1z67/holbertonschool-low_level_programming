05c4aa0cb3897d1bb37af04c580d09bc71efe9b6
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 18:43:07 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,26 +10,32 @@ void print_times_table(int n) {
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++) {
             int result = i * j;
-            char buffer[10];
+            char buffer[6];
             int len;
 
             if (result < 10) {
                 buffer[0] = ' ';
-                buffer[1] = result + '0';
-                len = 2;
+                buffer[1] = ' ';
+                buffer[2] = result + '0';
+                len = 3;
+            } else if (result < 100) {
+                buffer[0] = ' ';
+                buffer[1] = (result / 10) + '0';
+                buffer[2] = (result % 10) + '0';
+                len = 3;
             } else {
-                buffer[0] = result / 10 + '0';
-                buffer[1] = result % 10 + '0';
-                len = 2;
+                buffer[0] = (result / 100) + '0';
+                buffer[1] = ((result / 10) % 10) + '0';
+                buffer[2] = (result % 10) + '0';
+                len = 3;
             }
 
             buffer[len++] = ',';
             buffer[len++] = ' ';
 
-            
-            if (j != n && result < 10)
+            if (j != n)
                 buffer[len++] = ' ';
-            
+
             buffer[len] = '\0';
 
             write(1, buffer, len);
