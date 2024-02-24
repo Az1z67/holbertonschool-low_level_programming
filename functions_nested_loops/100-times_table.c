d5e6c9eff3a122b641776fa12df7717eadd60ae4
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 18:20:41 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@ void print_times_table(int n) {
     if (n < 0 || n > 15)
         return;
 
-    int i, j;
+    int i, j; // Переместим объявление переменных в начало функции
 
     for (i = 0; i <= n; i++) {
         for (j = 0; j <= n; j++) {
@@ -22,5 +22,5 @@ void print_times_table(int n) {
             }
         }
         putchar('\n');
-}
+    }
 }
