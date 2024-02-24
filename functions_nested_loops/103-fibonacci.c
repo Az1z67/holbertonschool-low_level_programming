fc78199adb52a19bf8029d731f88470ed8f09d1e
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 19:19:28 +0400

    commit

--- functions_nested_loops/103-fibonacci.c
+++ functions_nested_loops/103-fibonacci.c
@@ -4,14 +4,18 @@ int main(void) {
     int term1 = 1, term2 = 2, next_term = 0;
     long sum = 2;
 
-    while (next_term <= 4000000) {
+    while (term2 <= 4000000) {
         next_term = term1 + term2;
-        term1 = term2 + next_term;
-        term2 = next_term + term1;
-        sum += term2;
+        term1 = term2;
+        term2 = next_term;
+        
+        if (term2 % 2 == 0) {
+            sum += term2;
+        }
     }
 
     printf("%ld\n", sum);
 
     return 0;
 }
+
