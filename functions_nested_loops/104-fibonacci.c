5b0d0fd10bfe681bd5e6e87eab45cd8701d7fb3f
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 19:54:07 +0400

    commit

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 
-int main(void)
-{
+int main(void) {
     unsigned long a = 1, b = 2, c;
     int count;
 
     printf("1, 2, ");
 
-    for (count = 2; count <= 98; count++) {
+    for (count = 2; count < 98; count++) {
         c = a + b;
-        printf("%lu", c);
-        if (count < 98) {
-            printf(", ");
-        }
+        printf("%lu, ", c);
         a = b;
         b = c;
     }
-    printf("\n");
+
+    c = a + b;
+    printf("%lu\n", c);
 
     return 0;
 }
