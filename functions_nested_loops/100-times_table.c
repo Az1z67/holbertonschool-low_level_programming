f63764483bbefa41895525c9975684b4bbefba5a
Az1z67 <abbasovaziz61@gmail.com>
2024-02-24 18:45:32 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -2,15 +2,13 @@
 #include <unistd.h>
 
 void print_times_table(int n) {
-    int i, j;
-
     if (n < 0 || n > 15)
         return;
 
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
+    for (int i = 0; i <= n; i++) {
+        for (int j = 0; j <= n; j++) {
             int result = i * j;
-            char buffer[6];
+            char buffer[10]; // Adjusted buffer size
             int len;
 
             if (result < 10) {
