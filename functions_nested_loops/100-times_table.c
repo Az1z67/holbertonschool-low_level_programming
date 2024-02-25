627e963c9a430cd54c4e071474d5001b6cd05728
Az1z67 <abbasovaziz61@gmail.com>
2024-02-25 18:38:21 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
-/* Function prototype */
-void print_number(int n);
 
 /**
- * print_times_table - Prints the n times table
- * @n: The number to determine the size of the times table
+ * print_times_table - Prints the times table of the input,
+ *                     starting with 0.
+ * @n: The value of the times table to be printed.
  */
+
+void print_number(int n);
 void print_times_table(int n)
 {
     int i, j;
