a7848ea21e190bdee72ce497cce0bdf9827b0262
Az1z67 <abbasovaziz61@gmail.com>
2024-02-26 00:08:43 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -13,55 +13,53 @@ void print_number(int n);
  */
 void print_times_table(int n)
 {
-    int i, j;
+int i, j;
 
-    if (n < 0 || n > 15)
-        return;
-
-    for (i = 0; i <= n; i++)
-    {
-        for (j = 0; j <= n; j++)
-        {
-            int result = i * j;
-            if (j != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-            if (result >= 100)
-            {
-                _putchar((result / 100) + '0');
-                _putchar(((result / 10)) % 10 + '0');
-		_putchar(((result % 10)) + '0');
-            }
-            else if (result >= 10)
-            {
-                _putchar(' ');
-                print_number(result);
-            }
-            else
-            {
-                if (j != 0)
-                {
-                    _putchar(' ');
-                    _putchar(' ');
-                }
-                print_number(result);
-            }
-        }
-        _putchar('\n');
-    }
+if (n < 0 || n > 15)
+return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+int result = i * j;
+if (j != 0)
+{
+_putchar(',');
+_putchar(' ');
 }
-
-	void print_number(int n)
-	{
-    	if (n >= 10)
-    	{
-        	_putchar((n / 10) + '0');
-        	_putchar((n % 10) + '0');
-    	}
-    	else
-    	{
-        	_putchar(n + '0');
-    	}
-	}
+if (result >= 100)
+{
+_putchar((result / 100) + '0');
+_putchar(((result / 10)) % 10 + '0');
+_putchar(((result % 10)) + '0');
+}
+else if (result >= 10)
+{
+_putchar(' ');
+print_number(result);
+}
+else
+{
+if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+}
+print_number(result);
+}
+}
+_putchar('\n');
+}
+}
+void print_number(int n)
+{
+if (n >= 10)
+{
+_putchar((n / 10) + '0');
+_putchar((n % 10) + '0');
+}
+else
+{
+_putchar(n + '0');
+}
+}
