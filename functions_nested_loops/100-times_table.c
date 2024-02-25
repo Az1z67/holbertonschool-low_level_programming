6a6d9f503b14f1e7fb066d0b796829dbfac0b188
Az1z67 <abbasovaziz61@gmail.com>
2024-02-26 00:00:08 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -10,6 +10,12 @@ void print_number(int n);
  * print_times_table - Prints the times table of the input,
  * starting with 0.
  * @n: The value of the times table to be printed.
+ *
+ *
+ * print_number - Prints a number
+ * @n: The number to be printed
+ *
+ *
  */
 void print_times_table(int n)
 {
