5e982d2bd6f69a470be6f60d7b05df5b992d4475
Az1z67 <abbasovaziz61@gmail.com>
2024-02-25 15:38:38 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -1,24 +1,47 @@
 #include "main.h"
-#include <stdio.h>
 
-void print_times_table(int n) {
+
+/**
+ * print_times_table - Prints the n times table
+ * @n: The number to determine the size of the times table
+ */
+
+void print_times_table(int n)
+{
     int i, j;
 
-    if (n < 0 || n > 15) {
+    if (n < 0 || n > 15)
         return;
-    }
 
-    for (i = 0; i <= n; i++) {
-        for (j = 0; j <= n; j++) {
+    for (i = 0; i <= n; i++)
+    {
+        for (j = 0; j <= n; j++)
+        {
             int result = i * j;
-            printf("%d", result);
-
-            if (j < n) {
-                printf(", ");
-            } else {
-                printf("\n");
+            if (j != 0)
+            {
+                _putchar(',');
+                _putchar(' ');
+            }
+            if (result >= 100)
+            {
+                print_number(result);
+            }
+            else if (result >= 10)
+            {
+                _putchar(' ');
+                print_number(result);
+            }
+            else
+            {
+                if (j != 0)
+                {
+                    _putchar(' ');
+                    _putchar(' ');
+                }
+                print_number(result);
             }
         }
+        _putchar('\n');
     }
 }
-
