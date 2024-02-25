6011fb020365ec43fee798941ff7556e979a5b87
Az1z67 <abbasovaziz61@gmail.com>
2024-02-25 18:29:57 +0400

    commit

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -24,12 +24,11 @@ void print_times_table(int n)
                 _putchar(',');
                 _putchar(' ');
             }
-            if (prod >= 100)
+            if (prod = 100)
 	    {
-		    _putchar((prod / 100) + '0');
-		    _putchar(((prod / 10)) % 10 + '0');
+		    _putchar(prod);
+		    
 	    }
-	
             else if (result >= 10)
             {
                 _putchar(' ');
