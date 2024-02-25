9e99c078a649446e13b9910897862623f2b8ef67
Az1z67 <abbasovaziz61@gmail.com>
2024-02-25 15:02:22 +0400

    commit

--- functions_nested_loops/104-fibonacci.c
+++ functions_nested_loops/104-fibonacci.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 
-int main(void) {
-    unsigned long a = 1, b = 2, c;
-    int count;
+int main(void)
+{
+	int count;
+	unsigned long fibo1 = 1, fibo2 = 2, fibo_half1 = 0, fibo_half2 = 0;
+	unsigned long hold1, hold2, hold3;
 
-    printf("1, 2, ");
+	printf("%lu, %lu, ", fibo1, fibo2);
 
-    for (count = 2; count < 98; count++) {
-        c = a + b;
-        printf("%lu, ", c);
-        a = b;
-        b = c;
-    }
-
-    c = a + b;
-    printf("%lu\n", c);
-
-    return 0;
+for (count = 2; count < 98; count++);
+{
+	if (fibo1 + fibo2 > 10000000000 || fibo_half1 > 0 || fibo_half2 > 0)
+	{
+		hold1 = (fibo1 + fibo2) / 10000000000;
+		hold2 = (fibo1 + fibo2) % 10000000000;
+		hold3 = fibo_half1 + fibo_half2 + hold1;
+		fibo_half1 = fibo_half2, fibo_half2 = hold3;
+		fibo1 = fibo2, fibo2 = hold2;
+		printf("%lu%lu", fibo_half2, fibo2);
+	}
+	else
+	{
+		hold2 = fibo1 + fibo2;
+		fibo1 = fibo2, fibo2 = hold2;
+		printf("%lu", fibo2);
+	}
+	if (count != 97)
+        printf(", ");
+}
+printf("\n");
+return (0);
 }
