b0f47a05c7b3e485f19544beee4783f39b27aa65
Az1z67 <abbasovaziz61@gmail.com>
2024-02-28 01:56:31 +0400

    commit

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+* print_line - function that draws a line in terminal.
+*@n: int count of _
+*/
+void print_line(int n)
+{
+	int i;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 0; i < n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+}
