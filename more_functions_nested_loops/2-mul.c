33b651c2d17dabbb3cb227b41b8dfd979630a8b3
Az1z67 <abbasovaziz61@gmail.com>
2024-02-28 01:27:10 +0400

    commit

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * _mul - function that multiplies two integers
+ * @a: First int
+ * @b: Second int
+ * Return: anwser.
+ */
+
+int mul(int a, int b)
+{
+	int x;
+	x = a * b;
+	return (x);
+}
