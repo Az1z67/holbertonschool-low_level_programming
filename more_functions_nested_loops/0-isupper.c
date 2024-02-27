c004449e607f15bca90af1259c94e4db2c5a3bed
Az1z67 <abbasovaziz61@gmail.com>
2024-02-28 00:57:40 +0400

    commit

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ *_isupper function that checks for uppercase 
+ *@c: is a variable value
+ *
+ *Return: Always 0.
+ */
+
+
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
