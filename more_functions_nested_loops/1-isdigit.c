ed5a773b6fb879b6a5b841205f3de2760143c51a
Az1z67 <abbasovaziz61@gmail.com>
2024-02-28 01:08:27 +0400

    commit

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *
+ * _isdigit - is the function that check digits
+ * @c: is a variable value
+ *
+ * Return: 1 if it is digit and 0 if not
+ */
+
+int _isdigit(int c)
+{
+	if (c>='0' && c<='9')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
