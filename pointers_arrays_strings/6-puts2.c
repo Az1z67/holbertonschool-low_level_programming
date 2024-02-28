ab3d8bd30d1c200f1c369f6b594d98f1666278a1
Az1z67 <abbasovaziz61@gmail.com>
2024-02-29 01:45:01 +0400

    commit

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,18 +7,18 @@
 
 void puts2(char *str)
 {
-int len = 0;
+int leng = 0;
 int j;
 
-char *i = str;
+char *f = str;
 
 while (*i != '\0')
 {
-i++;
-len++;
+f++;
+leng++;
 }
-len = len - 1;
-for (j = 0; j <= len; j++)
+leng = leng - 1;
+for (j = 0; j <= leng; j++)
 {
 if (j % 2 == 0)
 {
