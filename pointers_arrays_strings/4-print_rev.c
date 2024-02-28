b16ff1692a87725e0f5045793381b8447580d823
Az1z67 <abbasovaziz61@gmail.com>
2024-02-29 00:55:46 +0400

    commit

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* reverse_array - function returns reverse of array
+* @a: takes in a character
+* @n:take in a char
+* Return: void
+*/
+
+void reverse_array(int *a, int n)
+{
+	int len;
+	
+	int i;
+	for (len = n - 1; len >= 0; len--)
+	{
+		_putchar(a[i]);
+	}
+	_putchar('\n');
+}
