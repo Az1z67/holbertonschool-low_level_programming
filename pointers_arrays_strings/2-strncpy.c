cca84de30ae8e53caf28f19e4f29ec5295e2bba7
Az1z67 <abbasovaziz61@gmail.com>
2024-02-29 22:26:19 +0400

    commit

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strncpy - function for copying
+ *@dest: str1
+ *@dest: str2
+ *@n: bytes
+ *Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	size_t i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+	return dest;
+}
