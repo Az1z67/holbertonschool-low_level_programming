1f33d8dc5fdf683e26bf109d102e2ee98cd16c45
Az1z67 <abbasovaziz61@gmail.com>
2024-02-29 22:17:17 +0400

    commit

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -16,13 +16,9 @@ char *_strncat(char *dest, char *src, int n)
 
 	while (dest[i] != '\0')
 		i++;
-	while (src[j] != '\0')
+	while (src[j] != '\0' && j < n)
 	{
 		dest[i] = src[j];
-		if (src[i] != n)
-		{
-			break;
-		}
 		j++;
 		i++;
 		dest[i] = '\0';
