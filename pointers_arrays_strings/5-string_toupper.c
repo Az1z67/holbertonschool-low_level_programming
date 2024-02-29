492701428f8fc0dbcfce05a11911a5c8382c2e51
Az1z67 <abbasovaziz61@gmail.com>
2024-02-29 23:33:26 +0400

    commit

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,15 @@
 #include "main.h"
+
 /**
  * string_toupper - function that change all lowercase to uppercase
  * @n: pointer
  * Return: n
  */
+
 char *string_toupper(char *n)
 {
 	int j = 0;
+	
 	while (n[j] != '\0')
 	{
 		if (n[j] >= 'a' && n[j] <= 'z')
