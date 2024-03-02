303896f6ccfcf60373ef6bd0e25289b39c6c05ee
Az1z67 <abbasovaziz61@gmail.com>
2024-03-02 16:28:14 +0400

    commit

---
+++ more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - finds and prints the largest prime factor of the number
+ * 612852475143
+ * Return: 0.
+ */
+
+int main(void)
+{
+	long num = 612852475143;
+	long div = 2;
+	long prime = 0;
+
+	while (num != 1)
+	{
+		if (num % div == 0)
+		{	
+			num = num / div;
+			prime = div;
+		}	
+		div += 1;
+	}
+	printf("%ld\n", prime);
+	return (0);
+
+}
