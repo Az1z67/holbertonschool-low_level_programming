9452cc81d8769b8c4e0a3bf312d98b9fcc509618
Az1z67 <abbasovaziz61@gmail.com>
2024-03-03 22:11:46 +0400

    commit

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define len 6
 
+/**
+ * main - Generate a random password for 101-crackme file
+ *
+ * Return: 0 (Success)
+ */
 
 int main(void)
 {
+	int password[64], i, sum = 0, n;
 
-int i;
-char password[len + 1];
-const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	srand(time(NULL));
+	for (i = 0; i < 64; i++)
+	{
+		password[i] = rand() % 78;
+		sum+= password[i] + '0';
+		putchar(password[i] + '0');
 
-srand(time(NULL));
-for (i = 0; i < len; ++i)
-{
-int index = rand() % (sizeof(charset) - 1);
-password[i] = charset[index];
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
+	}
+	return (0);
 }
-password[len] = '\0';
-printf("Generated Password: %s\n", password);
-return 0;
-}
-
