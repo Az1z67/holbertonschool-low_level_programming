a9bb9e871dec6f0788ca21aefd2e420be79210be
Az1z67 <abbasovaziz61@gmail.com>
2024-03-03 18:09:40 +0400

    commit

--- pointers_arrays_strings/101-crackme.c
+++ pointers_arrays_strings/101-crackme.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 #include <string.h>
 
-#define EXPECTED_PASSWORD "Password123"
+#define EXPECTED_PASSWORD "your_expected_password_here"
 
 int main(int a, char *b[]) {
-    if (a != 2)
-    {
+    if (a != 2) {
         printf("Usage: %s password\n", b[0]);
-        return (1);
+        return 1;
     }
 
     char *provided_password = b[1];
 
-    if (strcmp(provided_password, EXPECTED_PASSWORD) == 0)
-    {
+    if (strcmp(provided_password, EXPECTED_PASSWORD) == 0) {
         printf("Tada! Congrats\n");
-    }
-    else
-    {
+    } else {
         printf("Incorrect password\n");
     }
 
-    return (0);
+    return 0;
 }
+
