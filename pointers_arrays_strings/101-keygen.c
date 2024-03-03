7286ce3e452228515baaaecaaacfc39dd0254182
Az1z67 <abbasovaziz61@gmail.com>
2024-03-03 17:41:59 +0400

    commit

---
+++ pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define len 6
+
+int main(void)
+{
+char password[len + 1];
+const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+srand(time(NULL));
+
+for (int i = 0; i < len; ++i)
+{
+int index = rand() % (sizeof(charset) - 1);
+password[i] = charset[index];
+}
+password[len] = '\0';
+
+printf("Generated Password: %s\n", password);
+
+return 0;
+}
+
