a9a74f1a527347abe40090231ff3a683e5355ffb
Az1z67 <abbasovaziz61@gmail.com>
2024-03-06 22:24:12 +0400

    commit

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strchr - function that checks c
+ * @c: value
+ * @s: str
+ * Return: s or NULL
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; ; s++)
+	{
+		if (*s == 0)
+		{
+			return (s);
+		}
+		if (*s == 0)
+		{
+			return (NULL);
+		}
+	}
+}
