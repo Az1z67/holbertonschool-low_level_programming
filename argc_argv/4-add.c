287a68c7a307bfeb2a7e7d39a37942a4e744165b
Az1z67 <abbasovaziz61@gmail.com>
2024-03-08 02:35:55 +0400

    commit

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -20,3 +20,18 @@ int main(int argc, char *argv[])
 		printf("0\n");
 		return (0);
 	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (isdigit(argv[i][j]) == 0)
+			{
+				printf("Error\n");
+				return (0);
+			}
+		}
+		mul += atoi(argv[i]);
+	}
+	printf("%d\n", mul);
+	return (0);
+}
