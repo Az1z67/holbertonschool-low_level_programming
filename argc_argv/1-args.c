d312a1f7f76c55ffa3f4d20ee9b2edef807e13a5
Az1z67 <abbasovaziz61@gmail.com>
2024-03-08 02:28:31 +0400

    commit

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguenments
+ * @argc: arguemnet
+ * @argv: array
+ * Return: 0
+ **/
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
