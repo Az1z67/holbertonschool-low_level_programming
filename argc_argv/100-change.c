30a4333284032632055b9bbe2d806b57c22e6e1e
Az1z67 <abbasovaziz61@gmail.com>
2024-03-09 20:22:09 +0400

    commit

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -12,31 +12,33 @@
 
 int main(int argc, char *argv[])
 {
-        int a, b = 0, i, j;
-        int c[5] = {25, 10, 5, 2, 1};
+	int a, b = 0, i, j;
+	int c[5] = {25, 10, 5, 2, 1};
 
-        if (argc != 2)
-        {
-                puts("Error");
-                return (1);
-        }
-        a = atoi(argv[1]);
-        if (a <= 0)
-        {
-                puts("0");
-                return (1);
-        }
-        else
-        {
-                for (i = 0; i < 5; i++)
-                {
-                        j = a / c[i];
-                        a -= j * c[i];
-                        b += j;
-                        if (a == 0)
-                                break;
-                }
-        }
-        printf("%d\n", b);
-        return (0);
+	if (argc != 2)
+	{
+		puts("Error");
+		return (1);
+	}
+	a = atoi(argv[1]);
+	if (a <= 0)
+	{
+		puts("0");
+		return (1);
+
+	}
+	else
+	{
+		for (i = 0; i < 5; i++)
+		{
+			j = a / c[i];
+			a -= j * c[i];
+			b += j;
+			if (a == 0)
+				break;
+		}
+	}
+	printf("%d\n", b);
+	return (0);
 }
+
