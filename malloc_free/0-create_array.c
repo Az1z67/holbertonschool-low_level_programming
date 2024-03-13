d968fafc5b871b7620d1a41a4a00fbe96fb38b78
Az1z67 <abbasovaziz61@gmail.com>
2024-03-13 21:21:36 +0400

    commit

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,11 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
+
 /*
  * *create_array - creates an array
  * @size: size
  * @c: value
  * Return: str ot 0
  */
+
 char *create_array(unsigned int size, char c)
 {
 	char *str;
@@ -20,7 +22,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	i = 0;	
+	i = 0;
 	while (i < size)
 	{
 		str[i] = c;
