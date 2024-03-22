83a8d0a4fef1530da8c2182cc20c180d8e1ad17e
Az1z67 <abbasovaziz61@gmail.com>
2024-03-22 20:11:22 +0400

    commit

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,24 +1,21 @@
 #ifndef DOG_H
 #define DOG_H
 /**
- * struct dog - function to typedef struct dog
- * @name: character pointer
- * @age: float
- * @owner: character pointer
+ * struct dog - dog info
+ * @name: name
+ * @age: age
+ * @owner: owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
+};
+typedef struct dog dog_t;
 void _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
 void free_dog(dog_t *d);
 #endif
