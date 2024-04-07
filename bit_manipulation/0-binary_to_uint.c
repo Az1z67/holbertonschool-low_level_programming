07d66b64edc66c600cd73a52c0d8e7175b244a4a
Az1z67 <abbasovaziz61@gmail.com>
2024-04-07 17:31:20 +0400

    commit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -10,23 +10,30 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        unsigned int sum = 0;
-        unsigned int power = 1;
-        int i = 0;
+	unsigned int sum = 0;
+	unsigned int power = 1;
 
-        if (!b)
-                return (0);
-        while (b[i])
-                i++;
-        i--;
-        while (i >= 0)
-        {
-                if (b[i] != '0' && b[i] != '1')
-                        return (0);
-                if (b[i] == '1')
-                        sum += power;
-                i--;
-                power += power;
-        }
-        return (sum);
+	int i = 0;
+	
+	if (!b)
+		return (0);
+	while (b[i])
+	{
+		i++;
+		i--;
+	}
+	while (i >= 0)
+	{
+		if (b[i] != '0' && b[i] != '1')
+		{
+			return (0);
+		}
+		if (b[i] == '1')
+		{
+			sum += power;
+			i--;
+			power += power;
+		}
+	}
+	return (sum);
 }      
