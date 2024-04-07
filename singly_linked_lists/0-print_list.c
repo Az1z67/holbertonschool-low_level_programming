75665f5393aff126d1679847eb88b70e9afb8830
Az1z67 <abbasovaziz61@gmail.com>
2024-04-07 12:48:31 +0400

    commit

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * print_list - function that print list
+ * @h: pointer to the beginning of the list
+ * Return: count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
